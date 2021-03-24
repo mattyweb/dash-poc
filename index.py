1e079ce224dcab2d7b5fe1f00f55fd0c9a653c96
Matt Webster <me@matt-webster.net>
2021-03-24 15:11:23 -0700

    adding reloading selectors

--- index.py
+++ index.py
@@ -50,7 +50,7 @@ def display_page(pathname):
     elif last_part == 'reload':
         logger.log(logging.WARNING, "Report Server reload requested")
         os.kill(os.getpid(), signal.SIGTERM)
-        return html.Div('Reloading...')
+        return html.Div('Reloading...', id="reloading-msg", className="dash-reloading")
     
     # show links to each dashboard
     else:
